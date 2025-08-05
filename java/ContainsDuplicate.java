77c45ae27b4b038a7cf6210f8f8d0b9782adae42
Kamata954 <kimani.walker954@gmail.com>
2025-08-04 20:17:09 -0400

    Daily Coding Challenge 2025 08 04

---
+++ java/ContainsDuplicate.java
@@ -0,0 +1,16 @@
+package java;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ContainsDuplicate {
+    public boolean hasDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+
+        for(int num : nums){
+            set.add(num);
+        }
+
+        return !(set.size() == nums.length);
+    }
+}
